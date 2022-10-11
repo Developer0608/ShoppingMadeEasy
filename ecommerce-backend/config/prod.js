3fe5f13c1072cfc154efd813fedf2c27c81f737f
azadsingh99 <azadsingh1934278@gmail.com>
2022-10-11 07:43:02 +0530

    DB Migration

--- ecommerce-backend/config/prod.js
+++ ecommerce-backend/config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MONGO_URI: "mongodb+srv://Priyanka:Priyanka@#12345@cluster0.djdkczx.mongodb.net/testing",
+    MONGO_URI: "mongodb+srv://azadsingh:azadsingh123@cluster0.ecb3a91.mongodb.net/ShoppingMadeEasy",
 }
