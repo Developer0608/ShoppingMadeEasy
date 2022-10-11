a882c9226a7ad918401d707b0c28ba3b7107afd8
azadsingh99 <azadsingh1934278@gmail.com>
2022-10-11 08:46:36 +0530

    removed unused apps

--- ecommerce-backend/index.js
+++ ecommerce-backend/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
 const PORT = 8000;
 const userRouter = require('./routes/user');
-const { mongoDB } = require('./repository/mongoDbConfig');
+require('./repository/mongoDbConfig');
 
 
 app.use(userRouter);
