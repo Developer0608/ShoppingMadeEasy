d57323985d7c4360a875f1fb245c13e7985e07f9
azadsingh99 <azadsingh1934278@gmail.com>
2022-10-31 12:05:20 +0530

    Routes Setup

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route} from "react-router-dom";
 import Login from "./pages/login";
+import SignUp from './pages/signUp';
 
 function App() {
   console.log('IN APP');
   return <>
       <Routes> 
         <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp/>} />
      </Routes>
   </>
 }
---
+++ ecommerce/src/pages/signUp.js
@@ -0,0 +1,12 @@
+const signUp = () => {
+    console.log('I am SignUp');
+    return (
+        <>
+           <div>
+                <p>I am in SignUp</p>
+           </div>
+        </>
+    )
+}
+
+export default signUp;
